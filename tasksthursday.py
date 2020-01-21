7cf401e00c0a4abc4aeb54587ae9f486eb086169
ryasun85 <59563060+ryasun85@users.noreply.github.com>
2020-01-21 15:15:18 +0000

    Add files via upload

---
+++ tasksthursday.py
@@ -0,0 +1,18 @@
+def sandwich_order(topping1, topping2, topping3,topping4,topping5):
+
+    print("Your sandwich order is {},{}.".format(topping1, topping3))
+
+sandwich_order("ham","cheese", "olives", "tomatoes", "letucce")
+
+
+
+
+list_of_values = [
+    "value1",
+    "value2",
+    "value3"
+]
+
+list_of_values.insert(0, "Value5")
+
+print(list_of_values)
